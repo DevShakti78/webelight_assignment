55a14c3e03b53e5cb81cad9ba3ae68d9cdfd9c89
devshakti78 <shakti.dev46@gmail.com>
2022-09-02 21:33:17 +0530

    express server and simple product array

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+    {
+      id: 1,
+      name: "iPhone 12 Pro",
+      brand: "Apple",
+      desc: "6.1-inch display",
+      price: 999,
+      image:
+        "https://static.toiimg.com/thumb/resizemode-4,msid-78671027,imgsize-200,width-1200/78671027.jpg",
+    },
+    {
+      id: 2,
+      name: "iPhone 12",
+      brand: "Apple",
+      desc: "5.4-inch mini display",
+      price: 699,
+      image:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphone-12-purple-2021?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1635202738000",
+    },
+    {
+      id: 3,
+      name: "Galaxy S",
+      brand: "Samsung",
+      desc: "6.5-inch display",
+      price: 399,
+      image:
+        "https://www.91-img.com/pictures/109594-v2-samsung-galaxy-x-mobile-phone-large-1.jpg",
+    },
+  ];
+  
+  module.exports = products;
