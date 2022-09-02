6579375bd7c18befdd21a2e634bf243d710ff40e
devshakti78 <shakti.dev46@gmail.com>
2022-09-02 21:20:49 +0530

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import {BrowserRouter,Route,Routes,useNavigate} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <Route path="/cart/:id" element={<Cart />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" exact element={<Home/>} />
+
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
